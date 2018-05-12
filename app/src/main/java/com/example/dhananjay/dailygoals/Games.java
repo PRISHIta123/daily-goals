5a8c3eaee7fafc1a2bdc7b42ba93acf5d87e7402
PRISHIta123 <prishita.r85@gmail.com>
2018-05-12 15:41:42 +0530

    Added files for Opening windows for each Activity , Goals completed Value to be updated at each click

---
+++ app/src/main/java/com/example/dhananjay/dailygoals/Games.java
@@ -0,0 +1,13 @@
+package com.example.android.dailygoals;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Games extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.games);
+    }
+}
---
+++ app/src/main/java/com/example/dhananjay/dailygoals/Study1.java
@@ -0,0 +1,13 @@
+package com.example.android.dailygoals;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Study1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.study1);
+    }
+}
